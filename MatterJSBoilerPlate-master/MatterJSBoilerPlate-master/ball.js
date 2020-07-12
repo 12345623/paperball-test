56c65b5c388a9bfe0f0185d3cdf4caea2cc5750d
12345623 <67406829+12345623@users.noreply.github.com>
2020-07-12 14:42:25 +0530

    Add files via upload

---
+++ MatterJSBoilerPlate-master/MatterJSBoilerPlate-master/ball.js
@@ -0,0 +1,20 @@
+//class Ball{
+   // constructor(){
+     //   var options={
+    //        restitution:0.3,
+    //        isStatic:false,
+   //         friction:0.5,
+   //         density:1.2
+    //    }
+
+     //   this.body = Matter.Bodies.circle(200, 200, radius, options);
+    //    this.radius=this.radius
+     //   this.x=200
+    //    this.y=200
+        
+     //   World.add(world, ball);
+	
+
+  // }        
+    
+//}
